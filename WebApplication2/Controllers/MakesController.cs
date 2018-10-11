bafbedc2c7aa607a99ab1f47261450b5a96136ef
Yuhao Dong <ydong119@gmail.com>
2018-10-11 01:58:08 -0400

    seed database, first api, automapping, Resources class

---
+++ WebApplication2/Controllers/MakesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizApp.Controllers.Resources;
+using QuizApp.Models;
+using QuizApp.Persistence;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuizApp.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly QuizAppDbContext context;
+        private readonly IMapper mapper;
+
+        public MakesController(QuizAppDbContext context, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
+        }
+    }
+}
---
+++ WebApplication2/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using QuizApp.Controllers.Resources;
+using QuizApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
