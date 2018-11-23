99e90231d6d1c703cb227f68ade28fadc1977b60
Yuhao Dong <ydong119@gmail.com>
2018-11-22 22:14:43 -0500

    Basic server-side error handling

--- WebApplication2/ClientApp/src/app/app.module.ts
+++ WebApplication2/ClientApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
@@ -12,6 +12,7 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 import { VehicleService } from './services/vehicle.service';
+import { AppErrorHandler } from './app.error-handler';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { VehicleService } from './services/vehicle.service';
     ])
   ],
   providers: [
+    { provide: ErrorHandler, useClass: AppErrorHandler},
     VehicleService
   ],
   bootstrap: [AppComponent]
